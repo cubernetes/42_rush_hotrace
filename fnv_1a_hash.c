66e3af6096581a6d3866dd463fee5d6d92061449
astavrop <artemstavropolov@gmail.com>
2024-03-08 23:08:48 +0100

    docs: add comments to functions.

--- fnv_1a_hash.c
+++ fnv_1a_hash.c
@@ -6,12 +6,20 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 22:09:30 by astavrop          #+#    #+#             */
-/*   Updated: 2024/03/08 22:53:44 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/03/08 23:08:07 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hash_func.h"
 
+/**
+ * @brief Calculates a 32-bit FNV-1a hash from a given input string.
+ * 
+ * @param  *input_str: A pointer to a null-terminated string
+ *                     that is to be hashed.
+ * 
+ * @retval The 32-bit hash value as an unsigned integer.
+ */
 uint32_t	fnv_1a_32(char *input_str)
 {
 	unsigned char	*str;
@@ -28,6 +36,14 @@ uint32_t	fnv_1a_32(char *input_str)
 	return (hash);
 }
 
+/**
+ * @brief Calculates a 64-bit FNV-1a hash from a given input string.
+ * 
+ * @param  *input_str: A pointer to a null-terminated string
+ *                     that is to be hashed.
+ * 
+ * @retval The 64-bit hash value as an unsigned long integer.
+ */
 uint64_t	fnv_1a_64(char *input_str)
 {
 	unsigned char	*str;
